c09332af8a73edc5246909ee676123611c5bd16d
J-Hoogvliet <justinhoogvliet@outlook.com>
2025-02-01 00:08:49 +0100

    App routing okay

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import ChampionCollection from "./components/ChampionCollection.jsx";
+import ChampionDetail from "./components/ChampionDetail.jsx";
+import HomeScreen from "./components/Home.jsx";
+import {createBrowserRouter, RouterProvider} from "react-router";
+import CreateNewChampion from "./components/CreateNewChampion.jsx";
 function App() {
-  const [count, setCount] = useState(0)
+    const [champions, setChampions] = useState([])
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const router = createBrowserRouter([
+        {
+            // element: <layout/>,
+            children:[
+                {
+                    path: '/',
+                    element: <HomeScreen/>
+                },
+                {
+                    path: '/champions',
+                    element: <ChampionCollection champions={champions} setChampions={setChampions}/>
+                },
+                {
+                    path: '/champion/:id',
+                    element: <ChampionDetail/>
+                },
+                {
+                    path: '/champion/create',
+                    element: <CreateNewChampion/>
+                },
+
+            ]
+        }
+    ])
+
+    return (
+        <RouterProvider router={router}/>
+    );
 }
 
-export default App
+export default App;
